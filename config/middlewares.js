d7b86c0f0557d7a1e43c97b4f69ed45d664375ee
mravind2 <mohrav723@gmail.com>
2023-04-22 23:35:21 -0700

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,12 @@
-module.exports = {
-  load: {
-    before: ["responseTime", "logger", "cors", "responses"],
-    after: ["parser", "router"],
-  },
-  settings: {
-    cors: {
-      enabled: true,
-      origin: [
-        "https://designanoasis.vercel.app", // Replace with your Vercel app's URL
-        "http://localhost:3000", // Add this line to allow CORS for your local development environment
-      ],
-    },
-  },
-};
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
