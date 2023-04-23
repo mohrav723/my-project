5669ac2350f13698f02f028c16b27f31e047f2bb
mravind2 <mohrav723@gmail.com>
2023-04-23 00:30:18 -0700

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,14 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000', 'http://designanoasis.vercel.app']
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
